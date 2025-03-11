7e6b755f7273d46c7e40ec8ff8129d30fa8c114d
Aiden <weijlin850621@gmail.com>
2025-03-11 09:23:17 +0800

    Update astro.config.mjs
    調整 domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blog.weij0.div',
+  site: 'https://blog.weij.dev',
   base: '/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
